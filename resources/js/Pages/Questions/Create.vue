719ea300f28ff46d525ae6b1b8790e44a60df203
naquib555 <naquib555@gmail.com>
2022-10-23 17:19:57 -0400

    Dynamic option addition

--- resources/js/Pages/Questions/Create.vue
+++ resources/js/Pages/Questions/Create.vue
@@ -2,20 +2,13 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 import { useForm } from "@inertiajs/inertia-vue3";
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 
 const props = defineProps({
     questions: {
         type: Object,
         default: () => ({}),
-    },
-    addMore() {
-      this.form.options.push({
-        option: "",
-      });
-    },
-    remove(index) {
-      this.form.options.splice(index, 1);
-    },
+    }
 });
 
 
@@ -30,7 +23,8 @@ const form = useForm({
         {
             option: "",
         },
-    ]
+    ],
+    correctAnswer: []
     
 });
 
@@ -38,11 +32,34 @@ const addMore = () => {
     form.options.push({
         option: "",
       });
-    }
+}
 
 const remove = (index) => {
     form.options.splice(index, 1);
+}
+
+const optionType = () => {
+    
+    if (form.type === 'tf') {
+        form.options = []
+        form.options.push({option : 'True'})
+        form.options.push({option : 'False'})
+    } else {
+        form.options = [
+            {
+                option: "",
+            },
+            {
+                option: "",
+            },
+        ]
     }
+}
+
+const submit = () => {
+    console.log(form.options)
+    console.log(form.correctAnswer)
+}
 
 
 </script>
@@ -110,6 +127,7 @@ const remove = (index) => {
                                 <select 
                                     v-model="form.type"
                                     name="type"
+                                    @change="optionType()"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                     >
                                     <option value="tf">True or False</option>
@@ -122,46 +140,87 @@ const remove = (index) => {
                                     {{ form.errors.type }}
                                 </div>
                             </div>
-                            <div class="mb-6">
+                            <div class="mb-6" v-show="form.type === 'multiple'">
                                 <div v-for="(options, index) in form.options" :key="index">
                                     <label
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                             Option {{index + 1}}
                                     </label>
-                                    <textarea
-                                        type="text"
-                                        v-model="options.options"
-                                        name="content"
-                                        id=""
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                    ></textarea>
-                                    <button
-                                        type="button"
-                                        class="ml-2 rounded-md border px-3 py-2 bg-red-600 text-white"
-                                        @click="remove(index)"
-                                        v-show="form.options.length > 2"
-                                    >
-                                        Remove
-                                    </button>
+                                    <div class="grid grid-cols-12 gap-4">
+                                        <div class="col-span-11">
+                                            <textarea
+                                                type="text"
+                                                v-model="options.option"
+                                                name="option"
+                                                class="mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                            ></textarea>
+                                        </div>
+                                        <div class="justify-end">
+                                            <button
+                                                type="button"
+                                                class="px-3 py-1 rounded-md bg-red-600 text-white text-sm"
+                                                @click="remove(index)"
+                                                v-show="form.options.length > 2"
+                                            >
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                    
                                 </div>
                             </div>
-                            <div class="mb-6 flex justify-end">
+                            <div class="mb-6 flex justify-end" v-show="form.type === 'multiple'">
                                 <button
                                     type="button"
-                                    class="text-white bg-blue-700 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 "
+                                    class="text-white bg-blue-600 focus:outline-none  font-sm rounded-md text-sm px-3 py-1.5 "
                                     @click="addMore()"
                                     >
                                     Add New Option
                             </button>
                             </div>
-                            <button
+                            <div class="mb-6" v-show="form.type === 'multiple' || form.type === 'tf' ">
+                                <label
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                            Correct Answer
+                                    </label>
+                                    <div v-if="form.type === 'multiple'">
+                                        <select 
+                                            v-model="form.correctAnswer"
+                                            multiple="true"
+                                            name="correctAnswer"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                            >
+                                            <option v-for="(value, key) in form.options" 
+                                                    :value="key"
+                                                    :key="key">
+                                            Option {{key + 1}} : {{value.option}}
+                                            </option>
+                                        </select> 
+                                    </div>
+                                    <div v-else-if="form.type === 'tf'">
+                                        <select 
+                                            v-model="form.correctAnswer"
+                                            name="correctAnswer"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                            >
+                                            <option v-for="(value, key) in form.options" 
+                                                    :value="key"
+                                                    :key="key">
+                                            Option {{key + 1}} : {{value.option}}
+                                            </option>
+                                        </select> 
+                                    </div>
+                            </div>
+                            <PrimaryButton
                                 type="submit"
-                                class="text-white bg-blue-700  focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 "
+                                class="text-white focus:outline-none font-medium text-sm px-5 py-2.5 "
                                 :disabled="form.processing"
                                 :class="{ 'opacity-25': form.processing }"
                             >
                                 Submit
-                            </button>
+                            </PrimaryButton>
                         </form>
                     </div>
                 </div>
